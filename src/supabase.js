b3ea66b9f58790e5f31c9943d8e4ed96c3149b07
cristianoads91-sys <cristianoads91@gmail.com>
2026-05-04 11:31:56 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Credenciais do projeto Supabase
-const SUPABASE_URL = "import.meta.env.VITE_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "import.meta.env.VITE_SUPABASE_ANON_KEY";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
